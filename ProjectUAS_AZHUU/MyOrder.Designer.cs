1b46e5fefc6d77f7a839859c11c0fbcdfebe525b
Bepe09 <bputrantio@student.ciputra.ac.id>
2021-05-29 00:44:36 +0700

    Membuat form myorder

--- ProjectUAS_AZHUU/MyOrder.Designer.cs
+++ ProjectUAS_AZHUU/MyOrder.Designer.cs
@@ -29,19 +29,314 @@ namespace ProjectUAS_AZHUU
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelMyOrder = new System.Windows.Forms.Label();
+            this.labelPOBus1 = new System.Windows.Forms.Label();
+            this.labelOrderID1 = new System.Windows.Forms.Label();
+            this.labelTerminal1 = new System.Windows.Forms.Label();
+            this.labelStatusJalan1 = new System.Windows.Forms.Label();
+            this.labelTanggal1 = new System.Windows.Forms.Label();
+            this.labelStatusBayar1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelJam1 = new System.Windows.Forms.Label();
+            this.labelStatusBayar2 = new System.Windows.Forms.Label();
+            this.labelTanggal2 = new System.Windows.Forms.Label();
+            this.labelStatusJalan2 = new System.Windows.Forms.Label();
+            this.labelTerminal2 = new System.Windows.Forms.Label();
+            this.labelOrderID2 = new System.Windows.Forms.Label();
+            this.labelPOBus2 = new System.Windows.Forms.Label();
+            this.labelJam2 = new System.Windows.Forms.Label();
+            this.labelDetailOrder = new System.Windows.Forms.Label();
+            this.labelOrderID = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelID1 = new System.Windows.Forms.Label();
+            this.labelID2 = new System.Windows.Forms.Label();
+            this.labelTotalPembayaran = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.buttonContinueTOPay = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelMyOrder
+            // 
+            this.labelMyOrder.AutoSize = true;
+            this.labelMyOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMyOrder.Location = new System.Drawing.Point(100, 56);
+            this.labelMyOrder.Name = "labelMyOrder";
+            this.labelMyOrder.Size = new System.Drawing.Size(157, 39);
+            this.labelMyOrder.TabIndex = 0;
+            this.labelMyOrder.Text = "My Order";
+            // 
+            // labelPOBus1
+            // 
+            this.labelPOBus1.AutoSize = true;
+            this.labelPOBus1.Location = new System.Drawing.Point(107, 127);
+            this.labelPOBus1.Name = "labelPOBus1";
+            this.labelPOBus1.Size = new System.Drawing.Size(101, 17);
+            this.labelPOBus1.TabIndex = 1;
+            this.labelPOBus1.Text = "PO Bus Harjay";
+            // 
+            // labelOrderID1
+            // 
+            this.labelOrderID1.AutoSize = true;
+            this.labelOrderID1.Location = new System.Drawing.Point(107, 144);
+            this.labelOrderID1.Name = "labelOrderID1";
+            this.labelOrderID1.Size = new System.Drawing.Size(70, 17);
+            this.labelOrderID1.TabIndex = 2;
+            this.labelOrderID1.Text = "Order ID :";
+            // 
+            // labelTerminal1
+            // 
+            this.labelTerminal1.AutoSize = true;
+            this.labelTerminal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTerminal1.Location = new System.Drawing.Point(105, 161);
+            this.labelTerminal1.Name = "labelTerminal1";
+            this.labelTerminal1.Size = new System.Drawing.Size(253, 25);
+            this.labelTerminal1.TabIndex = 3;
+            this.labelTerminal1.Text = "Terminal 1 CGK -> BNI City";
+            // 
+            // labelStatusJalan1
+            // 
+            this.labelStatusJalan1.AutoSize = true;
+            this.labelStatusJalan1.Location = new System.Drawing.Point(107, 186);
+            this.labelStatusJalan1.Name = "labelStatusJalan1";
+            this.labelStatusJalan1.Size = new System.Drawing.Size(159, 17);
+            this.labelStatusJalan1.TabIndex = 4;
+            this.labelStatusJalan1.Text = "Sekali Jalan - 2 Dewasa";
+            // 
+            // labelTanggal1
+            // 
+            this.labelTanggal1.AutoSize = true;
+            this.labelTanggal1.Location = new System.Drawing.Point(107, 203);
+            this.labelTanggal1.Name = "labelTanggal1";
+            this.labelTanggal1.Size = new System.Drawing.Size(161, 17);
+            this.labelTanggal1.TabIndex = 5;
+            this.labelTanggal1.Text = "Kamis, 15 Agustus 2021";
+            // 
+            // labelStatusBayar1
+            // 
+            this.labelStatusBayar1.AutoSize = true;
+            this.labelStatusBayar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatusBayar1.Location = new System.Drawing.Point(106, 244);
+            this.labelStatusBayar1.Name = "labelStatusBayar1";
+            this.labelStatusBayar1.Size = new System.Drawing.Size(167, 20);
+            this.labelStatusBayar1.TabIndex = 6;
+            this.labelStatusBayar1.Text = "(Status Pembayaran)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(176, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 7;
+            // 
+            // labelJam1
+            // 
+            this.labelJam1.AutoSize = true;
+            this.labelJam1.Location = new System.Drawing.Point(285, 203);
+            this.labelJam1.Name = "labelJam1";
+            this.labelJam1.Size = new System.Drawing.Size(73, 17);
+            this.labelJam1.TabIndex = 8;
+            this.labelJam1.Text = "16.45 WIB";
+            // 
+            // labelStatusBayar2
+            // 
+            this.labelStatusBayar2.AutoSize = true;
+            this.labelStatusBayar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatusBayar2.Location = new System.Drawing.Point(106, 441);
+            this.labelStatusBayar2.Name = "labelStatusBayar2";
+            this.labelStatusBayar2.Size = new System.Drawing.Size(167, 20);
+            this.labelStatusBayar2.TabIndex = 14;
+            this.labelStatusBayar2.Text = "(Status Pembayaran)";
+            // 
+            // labelTanggal2
+            // 
+            this.labelTanggal2.AutoSize = true;
+            this.labelTanggal2.Location = new System.Drawing.Point(107, 400);
+            this.labelTanggal2.Name = "labelTanggal2";
+            this.labelTanggal2.Size = new System.Drawing.Size(161, 17);
+            this.labelTanggal2.TabIndex = 13;
+            this.labelTanggal2.Text = "Kamis, 15 Agustus 2021";
+            // 
+            // labelStatusJalan2
+            // 
+            this.labelStatusJalan2.AutoSize = true;
+            this.labelStatusJalan2.Location = new System.Drawing.Point(107, 383);
+            this.labelStatusJalan2.Name = "labelStatusJalan2";
+            this.labelStatusJalan2.Size = new System.Drawing.Size(159, 17);
+            this.labelStatusJalan2.TabIndex = 12;
+            this.labelStatusJalan2.Text = "Sekali Jalan - 2 Dewasa";
+            // 
+            // labelTerminal2
+            // 
+            this.labelTerminal2.AutoSize = true;
+            this.labelTerminal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTerminal2.Location = new System.Drawing.Point(105, 358);
+            this.labelTerminal2.Name = "labelTerminal2";
+            this.labelTerminal2.Size = new System.Drawing.Size(253, 25);
+            this.labelTerminal2.TabIndex = 11;
+            this.labelTerminal2.Text = "Terminal 1 CGK -> BNI City";
+            // 
+            // labelOrderID2
+            // 
+            this.labelOrderID2.AutoSize = true;
+            this.labelOrderID2.Location = new System.Drawing.Point(107, 341);
+            this.labelOrderID2.Name = "labelOrderID2";
+            this.labelOrderID2.Size = new System.Drawing.Size(70, 17);
+            this.labelOrderID2.TabIndex = 10;
+            this.labelOrderID2.Text = "Order ID :";
+            // 
+            // labelPOBus2
+            // 
+            this.labelPOBus2.AutoSize = true;
+            this.labelPOBus2.Location = new System.Drawing.Point(107, 324);
+            this.labelPOBus2.Name = "labelPOBus2";
+            this.labelPOBus2.Size = new System.Drawing.Size(101, 17);
+            this.labelPOBus2.TabIndex = 9;
+            this.labelPOBus2.Text = "PO Bus Harjay";
+            // 
+            // labelJam2
+            // 
+            this.labelJam2.AutoSize = true;
+            this.labelJam2.Location = new System.Drawing.Point(285, 400);
+            this.labelJam2.Name = "labelJam2";
+            this.labelJam2.Size = new System.Drawing.Size(73, 17);
+            this.labelJam2.TabIndex = 15;
+            this.labelJam2.Text = "16.45 WIB";
+            // 
+            // labelDetailOrder
+            // 
+            this.labelDetailOrder.AutoSize = true;
+            this.labelDetailOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDetailOrder.Location = new System.Drawing.Point(620, 56);
+            this.labelDetailOrder.Name = "labelDetailOrder";
+            this.labelDetailOrder.Size = new System.Drawing.Size(200, 39);
+            this.labelDetailOrder.TabIndex = 16;
+            this.labelDetailOrder.Text = "Detail Order";
+            // 
+            // labelOrderID
+            // 
+            this.labelOrderID.AutoSize = true;
+            this.labelOrderID.Location = new System.Drawing.Point(996, 78);
+            this.labelOrderID.Name = "labelOrderID";
+            this.labelOrderID.Size = new System.Drawing.Size(70, 17);
+            this.labelOrderID.TabIndex = 17;
+            this.labelOrderID.Text = "Order ID :";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(1073, 78);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(80, 17);
+            this.labelID.TabIndex = 18;
+            this.labelID.Text = "874512335";
+            // 
+            // labelID1
+            // 
+            this.labelID1.AutoSize = true;
+            this.labelID1.Location = new System.Drawing.Point(177, 144);
+            this.labelID1.Name = "labelID1";
+            this.labelID1.Size = new System.Drawing.Size(80, 17);
+            this.labelID1.TabIndex = 19;
+            this.labelID1.Text = "874512335";
+            // 
+            // labelID2
+            // 
+            this.labelID2.AutoSize = true;
+            this.labelID2.Location = new System.Drawing.Point(177, 341);
+            this.labelID2.Name = "labelID2";
+            this.labelID2.Size = new System.Drawing.Size(72, 17);
+            this.labelID2.TabIndex = 20;
+            this.labelID2.Text = "12345678";
+            // 
+            // labelTotalPembayaran
+            // 
+            this.labelTotalPembayaran.AutoSize = true;
+            this.labelTotalPembayaran.Location = new System.Drawing.Point(627, 126);
+            this.labelTotalPembayaran.Name = "labelTotalPembayaran";
+            this.labelTotalPembayaran.Size = new System.Drawing.Size(124, 17);
+            this.labelTotalPembayaran.TabIndex = 21;
+            this.labelTotalPembayaran.Text = "Total Pembayaran";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(630, 168);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(87, 17);
+            this.labelTotal.TabIndex = 22;
+            this.labelTotal.Text = "IDR 978.000";
+            // 
+            // buttonContinueTOPay
+            // 
+            this.buttonContinueTOPay.Location = new System.Drawing.Point(967, 137);
+            this.buttonContinueTOPay.Name = "buttonContinueTOPay";
+            this.buttonContinueTOPay.Size = new System.Drawing.Size(186, 31);
+            this.buttonContinueTOPay.TabIndex = 23;
+            this.buttonContinueTOPay.Text = "Continue To Pay";
+            this.buttonContinueTOPay.UseVisualStyleBackColor = true;
             // 
             // FormMyOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.buttonContinueTOPay);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelTotalPembayaran);
+            this.Controls.Add(this.labelID2);
+            this.Controls.Add(this.labelID1);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelOrderID);
+            this.Controls.Add(this.labelDetailOrder);
+            this.Controls.Add(this.labelJam2);
+            this.Controls.Add(this.labelStatusBayar2);
+            this.Controls.Add(this.labelTanggal2);
+            this.Controls.Add(this.labelStatusJalan2);
+            this.Controls.Add(this.labelTerminal2);
+            this.Controls.Add(this.labelOrderID2);
+            this.Controls.Add(this.labelPOBus2);
+            this.Controls.Add(this.labelJam1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelStatusBayar1);
+            this.Controls.Add(this.labelTanggal1);
+            this.Controls.Add(this.labelStatusJalan1);
+            this.Controls.Add(this.labelTerminal1);
+            this.Controls.Add(this.labelOrderID1);
+            this.Controls.Add(this.labelPOBus1);
+            this.Controls.Add(this.labelMyOrder);
             this.Name = "FormMyOrder";
             this.Text = "My Order";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelMyOrder;
+        private System.Windows.Forms.Label labelPOBus1;
+        private System.Windows.Forms.Label labelOrderID1;
+        private System.Windows.Forms.Label labelTerminal1;
+        private System.Windows.Forms.Label labelStatusJalan1;
+        private System.Windows.Forms.Label labelTanggal1;
+        private System.Windows.Forms.Label labelStatusBayar1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelJam1;
+        private System.Windows.Forms.Label labelStatusBayar2;
+        private System.Windows.Forms.Label labelTanggal2;
+        private System.Windows.Forms.Label labelStatusJalan2;
+        private System.Windows.Forms.Label labelTerminal2;
+        private System.Windows.Forms.Label labelOrderID2;
+        private System.Windows.Forms.Label labelPOBus2;
+        private System.Windows.Forms.Label labelJam2;
+        private System.Windows.Forms.Label labelDetailOrder;
+        private System.Windows.Forms.Label labelOrderID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelID1;
+        private System.Windows.Forms.Label labelID2;
+        private System.Windows.Forms.Label labelTotalPembayaran;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Button buttonContinueTOPay;
     }
 }
