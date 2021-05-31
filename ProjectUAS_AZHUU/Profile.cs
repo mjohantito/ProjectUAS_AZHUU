5d90de3b4c7e7d940a79065f00ebcdc4e95d93eb
Seo Dal-Mi <mjohantito@student.ciputra.ac.id>
2021-06-01 01:35:15 +0700

    oke brooo

--- ProjectUAS_AZHUU/Profile.cs
+++ ProjectUAS_AZHUU/Profile.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProjectUAS_AZHUU
 {
@@ -17,9 +18,24 @@ namespace ProjectUAS_AZHUU
             InitializeComponent();
         }
 
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        string connectString = "server=localhost;uid=root;pwd=;database=airport_shuttle;";
+        string sqlQuery;
+
         private void Profile_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                cboxTitle.Items.Add("Mr.");
+                cboxTitle.Items.Add("Mrs.");
+                cboxTitle.Items.Add("Ms.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- ProjectUAS_AZHUU/Sign-In.Designer.cs
+++ ProjectUAS_AZHUU/Sign-In.Designer.cs
@@ -45,46 +45,41 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(214, 437);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(160, 355);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "Email";
             // 
             // tboxEmail
             // 
-            this.tboxEmail.Location = new System.Drawing.Point(291, 437);
-            this.tboxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tboxEmail.Location = new System.Drawing.Point(218, 355);
             this.tboxEmail.Name = "tboxEmail";
-            this.tboxEmail.Size = new System.Drawing.Size(219, 22);
+            this.tboxEmail.Size = new System.Drawing.Size(165, 20);
             this.tboxEmail.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(214, 492);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(160, 400);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 11;
             this.label1.Text = "Password";
             // 
             // tboxPassword
             // 
-            this.tboxPassword.Location = new System.Drawing.Point(291, 492);
-            this.tboxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tboxPassword.Location = new System.Drawing.Point(218, 400);
             this.tboxPassword.Name = "tboxPassword";
-            this.tboxPassword.Size = new System.Drawing.Size(219, 22);
+            this.tboxPassword.Size = new System.Drawing.Size(165, 20);
             this.tboxPassword.TabIndex = 10;
             this.tboxPassword.TextChanged += new System.EventHandler(this.tboxPassword_TextChanged);
             // 
             // butSignIn
             // 
-            this.butSignIn.Location = new System.Drawing.Point(229, 555);
-            this.butSignIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.butSignIn.Location = new System.Drawing.Point(172, 484);
             this.butSignIn.Name = "butSignIn";
-            this.butSignIn.Size = new System.Drawing.Size(281, 31);
+            this.butSignIn.Size = new System.Drawing.Size(211, 25);
             this.butSignIn.TabIndex = 16;
             this.butSignIn.Text = "Sign-In";
             this.butSignIn.UseVisualStyleBackColor = true;
@@ -94,20 +89,18 @@
             // 
             this.labRegister.AutoSize = true;
             this.labRegister.ForeColor = System.Drawing.Color.DarkOrange;
-            this.labRegister.Location = new System.Drawing.Point(291, 763);
-            this.labRegister.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labRegister.Location = new System.Drawing.Point(218, 620);
             this.labRegister.Name = "labRegister";
-            this.labRegister.Size = new System.Drawing.Size(39, 17);
+            this.labRegister.Size = new System.Drawing.Size(30, 13);
             this.labRegister.TabIndex = 20;
             this.labRegister.Text = "Here";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(107, 762);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(80, 619);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(185, 17);
+            this.label8.Size = new System.Drawing.Size(142, 13);
             this.label8.TabIndex = 19;
             this.label8.Text = "Don\'t have account? Sign in";
             // 
@@ -115,29 +108,26 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Copperplate Gothic Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(397, 244);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(298, 198);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(237, 55);
+            this.label2.Size = new System.Drawing.Size(192, 44);
             this.label2.TabIndex = 21;
             this.label2.Text = "Sign - In";
             // 
             // labError
             // 
             this.labError.AutoSize = true;
-            this.labError.Location = new System.Drawing.Point(1084, 379);
-            this.labError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labError.Location = new System.Drawing.Point(182, 445);
             this.labError.Name = "labError";
-            this.labError.Size = new System.Drawing.Size(13, 17);
+            this.labError.Size = new System.Drawing.Size(10, 13);
             this.labError.TabIndex = 22;
             this.labError.Text = "-";
             // 
             // butForgotPass
             // 
-            this.butForgotPass.Location = new System.Drawing.Point(229, 614);
-            this.butForgotPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.butForgotPass.Location = new System.Drawing.Point(172, 532);
             this.butForgotPass.Name = "butForgotPass";
-            this.butForgotPass.Size = new System.Drawing.Size(281, 28);
+            this.butForgotPass.Size = new System.Drawing.Size(211, 23);
             this.butForgotPass.TabIndex = 23;
             this.butForgotPass.Text = "Forgot Password";
             this.butForgotPass.UseVisualStyleBackColor = true;
@@ -145,18 +135,19 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ProjectUAS_AZHUU.Properties.Resources.Purple_Space_Opera_Education_Presentation;
-            this.pictureBox1.Location = new System.Drawing.Point(-5, -4);
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1695, 862);
+            this.pictureBox1.Size = new System.Drawing.Size(1271, 700);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
             // 
             // Sign_In
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.butForgotPass);
             this.Controls.Add(this.labError);
             this.Controls.Add(this.labRegister);
@@ -168,7 +159,6 @@
             this.Controls.Add(this.tboxEmail);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Sign_In";
             this.Text = "Sign In";
             this.Load += new System.EventHandler(this.Sign_In_Load);
