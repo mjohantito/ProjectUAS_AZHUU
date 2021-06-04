eec7ddd19816eb06b9b5ff548a527d44409b8b47
Bepe09 <bputrantio@student.ciputra.ac.id>
2021-06-05 00:43:48 +0700

    blabla

--- ProjectUAS_AZHUU/CheckOut.cs
+++ ProjectUAS_AZHUU/CheckOut.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProjectUAS_AZHUU
 {
@@ -16,12 +17,25 @@ namespace ProjectUAS_AZHUU
         {
             InitializeComponent();
         }
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        string connectString = "server=localhost;uid=root;pwd=;database=airport_shuttle;";
+        string sqlQuery;
+
         int Potongan;
 
         private void buttonPay_Click(object sender, EventArgs e)
         {
             try
             {
+                DataTable dtProfile = new DataTable();
+                sqlConnect = new MySqlConnection(connectString);
+                sqlQuery = "";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtProfile);
+
                 FormMyOrder formMyOrder = new FormMyOrder();
                 formMyOrder.ShowDialog();
             }
