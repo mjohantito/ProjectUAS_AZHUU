7865afae42313f95f6d519e730115d06facba193
YohanWIjaya <75228962+YohanWIjaya@users.noreply.github.com>
2021-06-04 22:27:14 +0700

    ERAERQQ

--- ProjectUAS_AZHUU/CheckOut.Designer.cs
+++ ProjectUAS_AZHUU/CheckOut.Designer.cs
@@ -54,7 +54,7 @@ namespace ProjectUAS_AZHUU
             // 
             this.labelCart.AutoSize = true;
             this.labelCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCart.Location = new System.Drawing.Point(25, 31);
+            this.labelCart.Location = new System.Drawing.Point(110, 104);
             this.labelCart.Name = "labelCart";
             this.labelCart.Size = new System.Drawing.Size(91, 32);
             this.labelCart.TabIndex = 0;
@@ -63,7 +63,7 @@ namespace ProjectUAS_AZHUU
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 93);
+            this.label2.Location = new System.Drawing.Point(110, 166);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(150, 17);
             this.label2.TabIndex = 1;
@@ -72,7 +72,7 @@ namespace ProjectUAS_AZHUU
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(75, 127);
+            this.label3.Location = new System.Drawing.Point(160, 200);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(188, 17);
             this.label3.TabIndex = 2;
@@ -81,7 +81,7 @@ namespace ProjectUAS_AZHUU
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(75, 144);
+            this.label4.Location = new System.Drawing.Point(160, 217);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(186, 17);
             this.label4.TabIndex = 3;
@@ -90,7 +90,7 @@ namespace ProjectUAS_AZHUU
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(95, 183);
+            this.label5.Location = new System.Drawing.Point(180, 256);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 17);
             this.label5.TabIndex = 4;
@@ -99,7 +99,7 @@ namespace ProjectUAS_AZHUU
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(187, 183);
+            this.label6.Location = new System.Drawing.Point(272, 256);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(64, 17);
             this.label6.TabIndex = 5;
@@ -108,7 +108,7 @@ namespace ProjectUAS_AZHUU
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(187, 375);
+            this.label7.Location = new System.Drawing.Point(272, 448);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 17);
             this.label7.TabIndex = 10;
@@ -117,7 +117,7 @@ namespace ProjectUAS_AZHUU
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(95, 375);
+            this.label8.Location = new System.Drawing.Point(180, 448);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(80, 17);
             this.label8.TabIndex = 9;
@@ -126,7 +126,7 @@ namespace ProjectUAS_AZHUU
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(75, 336);
+            this.label9.Location = new System.Drawing.Point(160, 409);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(186, 17);
             this.label9.TabIndex = 8;
@@ -135,7 +135,7 @@ namespace ProjectUAS_AZHUU
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(75, 319);
+            this.label10.Location = new System.Drawing.Point(160, 392);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(188, 17);
             this.label10.TabIndex = 7;
@@ -144,7 +144,7 @@ namespace ProjectUAS_AZHUU
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(25, 285);
+            this.label11.Location = new System.Drawing.Point(110, 358);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(150, 17);
             this.label11.TabIndex = 6;
@@ -154,7 +154,7 @@ namespace ProjectUAS_AZHUU
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(25, 584);
+            this.label12.Location = new System.Drawing.Point(114, 584);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(68, 29);
             this.label12.TabIndex = 11;
@@ -164,7 +164,7 @@ namespace ProjectUAS_AZHUU
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(136, 584);
+            this.label13.Location = new System.Drawing.Point(225, 584);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(111, 29);
             this.label13.TabIndex = 12;
